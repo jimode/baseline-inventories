8bbc909a855f1640f0021e75e9a3787030d2786b
Jimi Odeyemi <jimode68@gmail.com>
2017-09-05 01:37:33 +0100

    Convert index.html to ejs template

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/src/views');
-// app.use(express.static('src')); //serves the index.html as the static file
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('views', __dirname + 'views');
+app.use(express.static('public')); //serves the index.html as the static file
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-   res.render('default', {title: 'Home'}); 
+   res.render('index', {title: 'Home'}); 
 });
 
 // app.get('/', function(req, res) {
@@ -14,14 +16,14 @@ app.get('/', function(req, res) {
 //  });
 
 app.get('/yemi', function (req, res) {
-    res.send('<h1>Hello Yemi<h1>')
+    res.send('<h1>Hello Yemi<h1>');
 });
 
 app.get('/who/:name?', function (req, res) {
     var name = req.params.name;
-    res.send(name + ' was here')
+    res.send(name + ' was here');
 });
 
-var server = app.listen(3001, function() {
-    console.log('Listening on port 3001');
+var server = app.listen(3000, function() {
+    console.log('Listening on port 3000');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -5,40 +5,40 @@ const gutil = require('gulp-util');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function () {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("public/css"))
         .pipe(browserSync.stream());
 });
 
-// Move JS Files to src/js
+// Move JS Files to public/js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js',
     'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
+        .pipe(gulp.dest("public/js"))
         .pipe(browserSync.stream());
 });
 
 // Watch Sass & Server
 gulp.task('serve', ['sass'], function(){
     browserSync.init({
-        server: "./src"
+        server: "./public"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'], ['sass']);
+    gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
-// Move Fonts Folder to src
+// Move Fonts Folder to public
 gulp.task('fonts', function(){
     return gulp.src('node_modules/font-awesome/fonts/*')
-        .pipe(gulp.dest("src/fonts"));
+        .pipe(gulp.dest("public/fonts"));
 });
 
-// Move Fonts Awesome CSS to src/css
+// Move Fonts Awesome CSS to public/css
 gulp.task('fa', function(){
     return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-        .pipe(gulp.dest("src/css"));
+        .pipe(gulp.dest("public/css"));
 });
 
 gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
